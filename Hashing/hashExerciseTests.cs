2988c118b12872e8f9fff07f951974227723d9b7
Carlo Yannuzzi <1carlogy.dev@gmail.com>
2025-06-11 07:23:51 -0400

    defined a PairSum method, that takes an int array and a target sum and returns an array of indeces of the two operands that sum to the target number. This method makes the assumption that there are two numbers within the array that sum to the target. This function has a time complexity of O(n) as we iteratate through the array looking for a complement operand when subtracting the element from the target. As we iterate through the array a hashmap is utilized to store the current element and it's index. If we find the complement already exists in the hashmap we know wehave a valid match and can return the complement's index and the current elements index.

--- Hashing/hashExerciseTests.cs
+++ Hashing/hashExerciseTests.cs
@@ -1,5 +1,4 @@
 namespace CS_DSA.Hashing;
-
 public static class HashTests
 {
     public static void TestAnagrams()
@@ -66,5 +65,43 @@ public static class HashTests
         {
             Console.WriteLine("🎉 All Tests Passed!");
         }
+    }
+
+    public static void TestPairSum()
+    {
+        var testsPassed = true;
+        var testCases = new Dictionary<Tuple<int[], int>, int[]>();
+        testCases.Add(new Tuple<int[], int>(new int[5] { 3, 2, 5, 4, 1 }, 8), new int[2] { 0, 2 });
+        testCases.Add(new Tuple<int[], int>(new int[6] { 4, 7, 9, 2, 5, 1 }, 5), new int[2] { 0, 5 });
+        testCases.Add(new Tuple<int[], int>(new int[6] { 4, 7, 9, 2, 5, 1 }, 3), new int[2] { 3, 5 });
+        testCases.Add(new Tuple<int[], int>(new int[4] { 1, 6, 7, 2 }, 13), new int[2] { 1, 2 });
+        testCases.Add(new Tuple<int[], int>(new int[2] { 9, 9 }, 18), new int[2] { 0, 1 });
+        testCases.Add(new Tuple<int[], int>(new int[4] { 6, 4, 2, 8 }, 12), new int[2] { 1, 3 });
+
+        var numbers = new List<int>();
+
+        for (var i = 0; i <= 30000; i++)
+        {
+            numbers.Add(i);
+        }
+        testCases.Add(new Tuple<int[], int>(numbers.ToArray(), 59999), new int[2] { 29999, 30000 });
+
+         foreach (var test in testCases)
+        {
+            var (arr, target) = test.Key;
+            Console.Write($"For:\t{(arr.Length < 10 ? String.Join(",", arr) : arr.Length)}\t{target}\tExpecting: {String.Join(",", test.Value)}\t");
+            var actual = HashingExercises.PairSum(arr, target);
+            if (!actual.SequenceEqual(test.Value))
+            {
+                testsPassed = false;
+                Console.Write($"Got:\t{String.Join(",", actual)}\t Test Failed ❌\n");
+            }
+            else { Console.Write("Test Passed ✅\n"); }
+        }
+        if (testsPassed)
+        {
+            Console.WriteLine("🎉 All Tests Passed!");
+        }
+
     }
 }
---
+++ Hashing/hashingExercises.cs
@@ -0,0 +1,76 @@
+namespace CS_DSA.Hashing;
+class HashingExercises
+{
+    public static bool Anagrams(string s1, string s2)
+    {
+        var s1Dict = new Dictionary<char, int>();
+
+        foreach (var item in s1)
+        {
+            if (!s1Dict.ContainsKey(item))
+            {
+                s1Dict.Add(item, 0);
+            }
+            s1Dict[item]++;
+        }
+
+        foreach (var item in s2)
+        {
+            if (!s1Dict.ContainsKey(item))
+            {
+                return false;
+            }
+
+            s1Dict[item]--;
+        }
+
+        foreach (var item in s1Dict)
+        {
+            if (item.Value != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static char MostFrequentChar(string s1)
+    {
+        var charCount = new Dictionary<char, int>();
+
+        foreach (var letter in s1)
+        {
+            if (!charCount.ContainsKey(letter))
+            {
+                charCount[letter] = 0;
+            }
+            charCount[letter]++;
+        }
+        var max = ValueTuple.Create<char, int>(' ', 0);
+
+        foreach (var letter in charCount)
+        {
+            if (letter.Value > max.Item2)
+            {
+                max = (letter.Key, letter.Value);
+            }
+        }
+        return max.Item1;
+    }
+
+    public static int[] PairSum(int[] nums, int target)
+    {
+        var count = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var complement = target - nums[i];
+            if (!count.ContainsKey(complement))
+            {
+                count[nums[i]] = i;
+                continue;
+            }
+            return [count[complement], i];
+        }
+        return [];
+    }
+}
